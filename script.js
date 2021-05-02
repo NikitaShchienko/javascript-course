0a1262006005a9a0a59a2e1953d33f8be7ea8c58
shchienko <shchienko@haulmont.com>
2021-05-03 01:30:03 +0400

    Added conditions and cycles

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 'use strict'
 
-const numberOfFilms = +prompt('How many films have you watched?', '');
-
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 }
 
-const lastFilm = prompt('What was the last movie you watched?', '');
+let count = 0;
+do {
+    count = +prompt('How many films have you watched?', 0);
+} while (count == 0);
 
-const lastFilmRating = +prompt('How do you rate this?', '');
+personalMovieDB.numberOfFilms = count
 
-personalMovieDB.movies[lastFilm] = lastFilmRating;
+for(let i = 0; i < 2; i++) { 
+    let lastFilm = null;
+    do { 
+        lastFilm = prompt('What was the last movie you watched?', '');
+    } while(lastFilm == null || lastFilm == '' || lastFilm.length == 0 || lastFilm.length > 50);
+
+    let lastFilmRating = null;
+    do {
+        lastFilmRating = +prompt('How do you rate this?', 0);
+    } while(lastFilmRating == null || lastFilmRating == 0 || lastFilmRating.length == 0 || lastFilmRating.length > 50);
+
+    personalMovieDB.movies[lastFilm] = lastFilmRating;
+}
+
+if (personalMovieDB.count > 0 && personalMovieDB.count <= 10) {
+    alert("You've watched quite a few films!");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert('You are a classical spectator!');
+} else if (personalMovieDB.count > 30) {
+    alert('You are a movie fan!');
+} else {
+    alert('Error!');
+}
 
 console.log(personalMovieDB);
 
